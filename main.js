8da660f8b9049f7566ee218c8729bc6f73c8bb34
Ashmita-03 <ashmitajyothilingam@gmail.com>
2025-10-15 18:14:12 +0000

    js

---
+++ main.js
@@ -0,0 +1,26 @@
+const screenDisplay = document.querySelector('.screen');
+const buttons = document.querySelectorAll('button');
+const clickSound = new Audio('clicksound.mp3');
+clickSound.preload = 'auto';
+
+let calculation = []
+let accumulativeCalculation
+
+// function for when you click the buttons
+function calculate(button) {
+    const value = button.textContent
+    if (value === "CLEAR") {
+        calculation = []
+        accumulativeCalculation="";
+        screenDisplay.textContent = ''
+    } else if (value === '=') {
+        accumulativeCalculation = calculation.join('');
+        console.log(accumulativeCalculation)
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+    calculation.push(value)
+    accumulativeCalculation = calculation.join('')
+    screenDisplay.textContent = accumulativeCalculation
+    }
+
+}
